7ab2961da5f3cb1c4f1bae181f43896fb185e3ef
cryptodeal <39316528+cryptodeal@users.noreply.github.com>
2022-10-27 23:34:47 -0500

    add basic cpp code for repro

--- bindings.cc
+++ bindings.cc
@@ -1,3 +1,34 @@
 #include <cstdlib>
 
-static std::unordered_set<fl::Tensor *> alreadyDestroyed;
+static std::unordered_set<void *> alreadyDestroyed;
+
+extern "C"
+{
+
+  void destroyBuffer(void *t, void * /*ignore*/)
+  {
+    auto *buffer = reinterpret_cast<float *>(t);
+    if (alreadyDestroyed.find(buffer) == alreadyDestroyed.end())
+    {
+      // std::cout << t << " was not found in `alreadyDestroyed`" << std::endl;
+      delete buffer;
+    }
+    else
+    {
+      // std::cout << "found " << t << " in `alreadyDestroyed`" << std::endl;
+      alreadyDestroyed.erase(alreadyDestroyed.find(buffer));
+    }
+  }
+
+  void *deallocationCtx()
+  {
+    return &alreadyDestroyed;
+  }
+
+  void dispose(void *t)
+  {
+    auto *buffer = reinterpret_cast<float *>(t);
+    alreadyDestroyed.insert(buffer);
+    delete buffer;
+  }
+}
